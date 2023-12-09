a43c6f4cfbfd5b849cbaf2c1e6d039799f443cb5
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-12-09 13:24:28 -0500

    Navs Working

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './Home'
 import Form from './Form'
 
 function App() {
   return (
-    <div id="app">
-      <nav>
-        {/* NavLinks here */}
-      </nav>
-      {/* Route and Routes here */}
-      <Home />
-      <Form />
-    </div>
-  )
+		<div id='app'>
+			<nav>
+				{/* NavLinks here */}
+				<Link to={'/'}>Home</Link>
+				<Link to={'order'}>Order</Link>
+			</nav>
+			{/* Route and Routes here */}
+			<Routes>
+				<Route path={'/'} element={<Home />} />
+				<Route path={'/order'} element={<Form />} />
+			</Routes>
+			<Home />
+			<Form />
+		</div>
+	);
 }
 
 export default App
